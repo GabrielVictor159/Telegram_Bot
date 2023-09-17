94137d5235f61c7f5a9254605b68a74b8132af18
Pedro Valladares <pedrohvalladares@hotmail.com>
2023-09-17 18:54:16 -0300

    estrutura do MVC

---
+++ src/Telegram.BOT.WebMVC/Controllers/GroupController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Telegram.BOT.Domain.Products;
+
+namespace Telegram.BOT.WebMVC.Controllers {
+    public class GroupController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult Create() {
+            var productGroups = new List<ProductGroups>();
+            return View(productGroups);
+        }
+
+        public IActionResult Edit() {
+            var productGroups = new List<ProductGroups>();
+            return View(productGroups); ;
+        }
+    }
+}
