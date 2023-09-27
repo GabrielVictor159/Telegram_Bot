1cc7f0f4cb2c8497d3d4c19a57d448834b4be22b
Gabriel Victor <107073417+GabrielVictor159@users.noreply.github.com>
2023-09-27 11:25:18 -0300

    Desenvolvendo testes dos casos de uso do category e marc

---
+++ src/Telegram.BOT.tests/Cases/Application/Category/DeleteCategory/DeleteCategoryUseCaseTest.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Server.HttpSys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.BOT.Application.Interfaces.Repositories;
+using Telegram.BOT.Application.UseCases.Category.DeleteCategory;
+using Telegram.BOT.tests.Builder.Domain.Product;
+using Xunit;
+using Xunit.Frameworks.Autofac;
+
+namespace Telegram.BOT.tests.Cases.Application.Category.DeleteCategory;
+
+[UseAutofacTestFramework]
+public class DeleteCategoryUseCaseTest
+{
+    private readonly DeleteCategoryUseCase deleteCategoryUseCase;
+    private readonly ICategoryRepository categoryRepository;
+    public DeleteCategoryUseCaseTest
+        (DeleteCategoryUseCase deleteCategoryUseCase,
+        ICategoryRepository categoryRepository)
+    {
+        this.deleteCategoryUseCase = deleteCategoryUseCase;
+        this.categoryRepository = categoryRepository;
+    }
+    [Fact]
+    public async Task ShouldSucess()
+    {
+        var entity = CategoryBuilder.New().Build(); 
+        categoryRepository.Add(entity);
+        var request = new DeleteCategoryRequest() { Id = Guid.NewGuid() };
+        await deleteCategoryUseCase.Execute(request);
+        request.output.Should().NotBeNull();
+        request.IsError.Should().BeFalse();  
+    }
+}
---
+++ src/Telegram.BOT.tests/Cases/Application/Marc/CreateMarc/Handlers/ValidateMarcHandlerTest.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.BOT.Application.Interfaces.Services;
+using Telegram.BOT.Application.UseCases.Marc.CreateMarc;
+using Telegram.BOT.Application.UseCases.Marc.CreateMarc.Handlers;
+using Telegram.BOT.tests.Builder.Domain.Product;
+using Xunit;
+using Xunit.Frameworks.Autofac;
+
+namespace Telegram.BOT.tests.Cases.Application.Marc.CreateMarc.Handlers;
+[UseAutofacTestFramework]
+public class ValidateMarcHandlerTest
+{
+    private readonly ValidateMarcHandler validateMarcHandler;
+    private readonly INotificationService notificationService;
+    public ValidateMarcHandlerTest(ValidateMarcHandler validateMarcHandler, INotificationService notificationService)
+    {
+        this.validateMarcHandler = validateMarcHandler;
+        this.notificationService = notificationService;
+    }
+    [Fact]
+    public async Task ShouldSucessValidDomain()
+    {
+        await validateMarcHandler.ProcessRequest(new CreateMarcRequest() { marc = MarcBuilder.New().Build() });
+        notificationService.HasNotifications.Should().BeFalse();
+    }
+    [Fact]
+    public async Task ShouldFailedInValidDomain()
+    {
+        await validateMarcHandler.ProcessRequest(new CreateMarcRequest() { marc = MarcBuilder.New().WithName("").Build() });
+        notificationService.HasNotifications.Should().BeTrue();
+    }
+}
