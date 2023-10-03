0f8b119483cb2ab950d2256b1a397aed72f118df
Pedro Valladares <pedrohvalladares@hotmail.com>
2023-10-02 21:11:39 -0300

    adicionando serviço de gerenciamento de arquivos e senha

---
+++ src/Telegram.BOT.Application/Interfaces/Services/IFileManagementService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telegram.BOT.Application.Interfaces.Services {
+    public interface IFileManagementService 
+    {
+        string SaveFile(string name, string content);
+        string getFileContent(string fileName);
+        void DeleteFile(string fileName);
+    }
+}
---
+++ src/Telegram.BOT.Application/Interfaces/Services/IPasswordHashService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telegram.BOT.Application.Interfaces.Services {
+    public interface IPasswordHashService {
+        string Hash(string password);
+        bool VerifyPassword(string password, string hashedPassword);
+    }
+}
