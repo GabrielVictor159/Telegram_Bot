4d1310d7a06534403dc7558608df62b1bb6b6979
Pedro Valladares <pedrohvalladares@hotmail.com>
2023-10-03 21:17:49 -0300

    refatorando código de validação dos campos

--- src/Telegram.BOT.WebMVC/wwwroot/js/site.js
+++ src/Telegram.BOT.WebMVC/wwwroot/js/site.js
@@ -1,3 +1,21 @@
-﻿const form = new Form(document.getElementsByClassName("validate").item(0))
+﻿const validableForms = Array.from(document.getElementsByClassName("validate"))
+if (validableForms != null) {
+    validableForms.forEach(form => {
+        let formValidator = new FormValidator(form);
+        let isInvalid = false
+        form.addEventListener("submit", event => {
+            if (formValidator.allInputGroups.length > 0) {
+                isInvalid = formValidator.hasEmpty()
+            }
+            if (formValidator.numericInputGroups.length > 0 && !isInvalid) {
+                isInvalid = formValidator.hasInvalidNumbers()
+            }
+            if (isInvalid) {
+                event.preventDefault()
+                formValidator.addRealTimeContentValidation()
+            }
+        })
+    })
+}
 
 
