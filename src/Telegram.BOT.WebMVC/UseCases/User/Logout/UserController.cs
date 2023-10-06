4933b9774772f0498db9158847973db9aec7cfe1
Pedro Valladares <pedrohvalladares@hotmail.com>
2023-10-06 19:59:56 -0300

    restruturando UserController

---
+++ src/Telegram.BOT.WebMVC/UseCases/User/Logout/UserController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Telegram.BOT.WebMVC.UseCases.User.Logout {
+    public class UserController : Controller {
+
+        public IActionResult Logout() {
+            throw new NotImplementedException();
+        }
+    }
+}
