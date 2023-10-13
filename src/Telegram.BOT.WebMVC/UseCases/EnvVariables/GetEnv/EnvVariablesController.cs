1ac685ec178913a30708100afa0687e18c761e86
Pedro Valladares <pedrohvalladares@hotmail.com>
2023-10-12 21:10:38 -0300

    adicionando telas de variaveis de ambiente

--- src/Telegram.BOT.WebMVC/UseCases/EnvVariables/GetEnv/EnvVariablesController.cs
+++ src/Telegram.BOT.WebMVC/UseCases/EnvVariables/GetEnv/EnvVariablesController.cs
@@ -1,9 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Telegram.BOT.Application.UseCases.Ambient.EnvVariables.GetEnvByFilter;
 
 namespace Telegram.BOT.WebMVC.UseCases.EnvVariables.GetEnv {
     public class EnvVariablesController : Controller {
+
+        private readonly IGetEnvByFilterRequest getEnvByFilterRequest;
+
+        public EnvVariablesController(IGetEnvByFilterRequest getEnvByFilterRequest) {
+            this.getEnvByFilterRequest = getEnvByFilterRequest;
+        }
+
         public IActionResult Index() {
-            return View();
+            var envRequest = new Application.UseCases.Ambient.EnvVariables.GetEnvByFilter.GetEnvByFilterRequest();
+            getEnvByFilterRequest.Execute(envRequest);
+
+            if(!envRequest.IsError && envRequest.output != null) {
+                return View(new GetEnvByFilterResponse { envVariables = envRequest.variablesFound });
+            } else {
+                return View("Error");
+            }
         }
     }
 }
