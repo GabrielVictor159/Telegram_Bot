76d495ea30f689039aea3f61e3270bf59ca1cb01
Pedro Valladares <pedrohvalladares@hotmail.com>
2023-10-14 12:20:30 -0300

    implementando funcionalidades dos Products

--- src/Telegram.BOT.WebMVC/UseCases/Products/DeleteProducts/ProductsController.cs
+++ src/Telegram.BOT.WebMVC/UseCases/Products/DeleteProducts/ProductsController.cs
@@ -1,13 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using Telegram.BOT.Application.Interfaces.Repositories;
+using Telegram.BOT.Application.UseCases.Products.DeleteProduct;
 using Telegram.BOT.Domain.Products;
 
 namespace Telegram.BOT.WebMVC.UseCases.Products.DeleteProducts
 {
     public class ProductsController : Controller
     {
-        public ActionResult Delete() {
-            throw new NotImplementedException();
+
+        private readonly IDeleteProductRequest deleteProductRequest;
+
+        public ProductsController(IDeleteProductRequest deleteProductRequest) {
+            this.deleteProductRequest = deleteProductRequest;
+        }
+
+        public IActionResult Delete(string itemid) {
+            var deleteRequest = new Application.UseCases.Products.DeleteProduct.DeleteProductRequest(){ Id = Guid.Parse(itemid) };
+            deleteProductRequest.Execute(deleteRequest);
+            if (!deleteRequest.IsError && deleteRequest.output != null) {
+                return LocalRedirect("/Products");
+            } else {
+                return View("Error", (deleteRequest.ErrorMessage, "Index"));
+            }
         }
     }
 }
--- src/Telegram.BOT.WebMVC/UseCases/Products/GetProducts/ProductsController.cs
+++ src/Telegram.BOT.WebMVC/UseCases/Products/GetProducts/ProductsController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Telegram.BOT.Application.Interfaces.Repositories;
-using Telegram.BOT.Application.UseCases.Products.CreateProduct;
-using Telegram.BOT.Application.UseCases.Products.DeleteProduct;
+using Telegram.BOT.Application.UseCases.Marc.GetMarc;
 using Telegram.BOT.Application.UseCases.Products.GetProduct;
 using Telegram.BOT.Domain.Products;
 using Telegram.BOT.WebMVC.UseCases.Marc.GetMarc;
@@ -12,13 +11,9 @@ namespace Telegram.BOT.WebMVC.UseCases.Products.GetProducts
     {
 
         private readonly IGetProductRequest getProductRequest;
-        private readonly ICreateProductRequest createProductRequest;
-        private readonly IDeleteProductRequest deleteProductRequest;
 
-        public ProductsController(IGetProductRequest getProductRequest, ICreateProductRequest createProductRequest, IDeleteProductRequest deleteProductRequest) {
+        public ProductsController(IGetProductRequest getProductRequest) {
             this.getProductRequest = getProductRequest;
-            this.createProductRequest = createProductRequest;
-            this.deleteProductRequest = deleteProductRequest;
         }
 
         [HttpGet]
@@ -37,9 +32,26 @@ namespace Telegram.BOT.WebMVC.UseCases.Products.GetProducts
             }
         }
 
-        [HttpPost]
         public IActionResult Search(GetProductRequest request) {
-            throw new NotImplementedException();
+            var searchRequest = new Application.UseCases.Products.GetProduct.GetProductRequest();
+            getProductRequest.Execute(searchRequest);
+
+            if (!searchRequest.IsError && searchRequest.output != null) {
+                var products = searchRequest.Products
+                .Select(c => new ProductResponse(c.Id, c.Name, c.Description, c.Image, c.Tags, c.CreateDate, c.Price, c.Marc!, c.MarcId))
+                .Where(c => c.name.Contains(request.Name ?? "") && c.tags.Contains(request.Tags ?? "") && c.description.Contains(request.Description ?? ""))
+                .Where(c => {
+                    if(request.filterEndDate == DateTime.MinValue) {
+                        return DateTime.Compare(c.CreateDate, request.filterStartDate) > 0 && DateTime.Compare(c.CreateDate, DateTime.MaxValue) < 0;
+                    } else {
+                        return DateTime.Compare(c.CreateDate, request.filterStartDate) > 0 && DateTime.Compare(c.CreateDate, request.filterEndDate) < 0;
+                    }
+                })
+                .ToList();
+                return View("Index", new GetProductResponse() { Products = products });
+            } else {
+                return View("Error");
+            }
         }
 
     }
