b6d860168bdf47c762bb034144fd26be6822627a
Pedro Valladares <pedrohvalladares@hotmail.com>
2023-10-15 10:27:53 -0300

    arrumando autênticação

---
+++ src/Telegram.BOT.WebMVC/UseCases/User/Forbidden/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Telegram.BOT.WebMVC.UseCases.User.Forbidden {
+    [AllowAnonymous]
+    public class UserController : Controller {
+        public IActionResult Forbidden() {
+            return View();
+        }
+    }
+}
